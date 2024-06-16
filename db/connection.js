90f9ef5ae6c6db9e73c1140daed0399d3445b019
vamsiswararao <yellapuv23@gmail.com>
2024-06-16 06:55:31 +0530

    modified the db connection

--- db/connection.js
+++ db/connection.js
@@ -3,7 +3,10 @@ require("dotenv").config()
 
 // const url = "mongodb+srv://chat_app_admin:9133584435@cluster0.kxl3855.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 
-mongoose.connect(process.env.MONGO_URL).then(()=>{
+mongoose.connect(process.env.MONGO_URL ,{
+    useNewUrlParser:true,
+    useUnifiedTopology:true,
+}).then(()=>{
     console.log("connected to the DB")
 }).catch((err)=>{
     console.log("error:", err)
